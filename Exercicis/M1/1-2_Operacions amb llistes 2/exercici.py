91d026c1272b04d576f317e79743c775f3e07d92
Jordi Casas-Roma <Jcasasr@uoc.edu>
2026-02-12 11:50:16 +0100

    Correcció Exercici M1_1-2 test-2b

--- Exercicis/M1/1-2_Operacions amb llistes 2/exercici.py
+++ Exercicis/M1/1-2_Operacions amb llistes 2/exercici.py
@@ -1,13 +1,14 @@
 def interseccio(llista1, llista2):
-
+    pass
 
 def unio(llista1, llista2):
-
+    pass
 
 def multiplicacio_llistes(llista1, llista2):
-
+    pass
 
 def multiplicacio_elements(llista1, llista2):
-
+    pass
 
 def distancia_hamming(binari1, binari2):
+    pass
